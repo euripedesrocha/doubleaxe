76e63864d6d45f9bdd4a61ce2f4c68ef0da10410
Euripedes Rocha <rocha.euripedes@gmail.com>
2017-11-15 07:30:41 -0200

    Adds more test cases and implement the permutation.
    - Adds 2 more test case to rotate
    - Implement the permutation transcripted from:
      https://eprint.iacr.org/2017/630.pdf

--- gimli/gimli.cpp
+++ gimli/gimli.cpp
@@ -4,5 +4,39 @@ namespace Gimli {
 uint32_t rotate(uint32_t word, uint8_t nbits) {
   return (word << nbits) | (word >> (32 - nbits));
 }
-void permutation(GimliState& state);
+void permutation(GimliState& state) {
+  uint32_t x = 0;
+  uint32_t y = 0;
+  uint32_t z = 0;
+
+  for (auto round = 24; round > 0; --round) {
+    for (auto column = 0; column < 4; ++column{
+      x = rotate(state[column], 24);
+      y = rotate(state[4 + column], 9);
+      z = state[8 + column];
+      state[8 + column] = x ^ (z << 1) ^ ((y & z) << 2);
+      state[4 + column] = y ^ x ^ ((x | z) << 1);
+      state[column] = z ^ y ^ ((x & y) << 3);
+    }
+    if ((round &3)==0){
+      x = state[0];
+      state[0] = state[1];
+      state[1] = x;
+      x = state[2];
+      state[2] = state[3];
+      state[3] = x;
+    }
+    if ((round &3)==2){
+      x = state[0];
+      state[0] = state[2];
+      state[2] = x;
+      x = state[1];
+      state[1] = state[3];
+      state[3] = x;
+    }
+    if ((round &3)==0){
+      state[0] ^= (0x9e377900 | round);
+    }
+  }
+}
 }  // namespace Gimli
--- gimli/test/test_gimli.cpp
+++ gimli/test/test_gimli.cpp
@@ -5,4 +5,6 @@ SCENARIO("Words for rotation") {
   REQUIRE(Gimli::rotate(0, 5) == 0);
   REQUIRE(Gimli::rotate(0xBADC0FFE, 0) == 0xBADC0FFE);
   REQUIRE(Gimli::rotate(0xBADC0FFE, 4) == 0xADC0FFEB);
+  REQUIRE(Gimli::rotate(0xBADC0FFE, 24) == 0xFEBADC0F);
+  REQUIRE(Gimli::rotate(0xBADC0FFE, 9) == 0xB81FFD75);
 }
