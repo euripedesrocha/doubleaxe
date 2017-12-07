776bcf912262211a9b513f7b4d31594ab3b1d07f
Euripedes Rocha <euripedes@ciandt.com>
2017-12-07 07:33:50 -0200

    Adapts Gimli primitive to receive a structure and.
    - Makes Gimli impl CRTP ready.

--- primitives/gimli/test/test_gimli.cpp
+++ primitives/gimli/test/test_gimli.cpp
@@ -6,29 +6,30 @@ extern "C" {
 #include "gimli.h"
 }
 
-void RandomState(Gimli::State& data) {
+using GimliState = std::array<uint32_t, 12>;
+void RandomState(GimliState& data) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<uint32_t> rand_uint32;
   std::generate(std::begin(data), std::end(data),
                 [&]() { return rand_uint32(gen); });
 }
-SCENARIO("Words for rotation") {
-  REQUIRE(Gimli::rotate(0, 5) == 0);
-  REQUIRE(Gimli::rotate(0xBADC0FFE, 0) == 0xBADC0FFE);
-  REQUIRE(Gimli::rotate(0xBADC0FFE, 4) == 0xADC0FFEB);
-  REQUIRE(Gimli::rotate(0xBADC0FFE, 24) == 0xFEBADC0F);
-  REQUIRE(Gimli::rotate(0xBADC0FFE, 9) == 0xB81FFD75);
-}
+class HasGimli : Gimli::Gimli<HasGimli, GimliState, 24> {
+ public:
+  void permutation(GimliState& state) {
+    static_cast<Gimli*>(this)->permutation(state);
+  }
+};
 
 SCENARIO("A full state for rotation") {
   GIVEN("A randomly generated state") {
-    Gimli::State InitialData;
+    HasGimli MyGimli;
+    GimliState InitialData;
     RandomState(InitialData);
     auto ReferenceData = InitialData;
     WHEN("The state is processed by my implementation") {
       gimli(std::begin(ReferenceData));
-      Gimli::permutation(InitialData);
+      MyGimli.permutation(InitialData);
       THEN("The result should be the same of the reference") {
         REQUIRE(ReferenceData == InitialData);
       }
